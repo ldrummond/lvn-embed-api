46c03494bd175224d9012c20e04461f3c7f86ad6
Tevin-Brown <tevin@cortico.ai>
2022-04-07 15:24:46 -0400

    updating with production highlights

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -8,17 +8,15 @@ function App() {
 
   React.useEffect(() => {
     // Will place the embed inside the element with the given id
-    new HighlightPlayer("embed", 161, { environment: "dev" });
+    new HighlightPlayer("embed", 2726220);
     // Allows for custom
     setMinimalPlayer(
-      new HighlightPlayer("embed-minimal", 161, {
-        environment: "dev",
+      new HighlightPlayer("embed-minimal", 2726220, {
         type: "minimal",
-        src: "http://0.0.0.0:10000",
         width: "570",
         height: "210",
         textColor: "#ffffff",
-        backgroundColor: "#494848",
+        backgroundColor: "#2a2a2a",
         highlightColor: "#87CB9C",
         fontSize: "20px",
         fontWeight: "bold",
@@ -50,10 +48,24 @@ function App() {
           justifyContent: "center",
         }}
       >
-        <button onClick={handlePlayToggle}>
-          {isPlaying ? "Pause" : "Play"}
-        </button>
-        <div className="App" id="embed-minimal"></div>
+        <div
+          style={{
+            border: "2px solid #87CB9C",
+            padding: "2rem",
+            paddingBottom: "0",
+            borderRadius: "8px",
+            backgroundColor: "#2a2a2a",
+            color: "#ffffff",
+          }}
+        >
+          <div style={{ marginBottom: "1rem" }}>
+            <button onClick={handlePlayToggle} style={{ marginRight: "1rem" }}>
+              {isPlaying ? "Pause" : "Play"}
+            </button>
+            <span>Tony</span>
+          </div>
+          <div className="App" id="embed-minimal"></div>
+        </div>
       </div>
     </>
   );
